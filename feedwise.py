200de77b7699646ecdb5c39e1601a77320029819
Adam Sampson <ats@offog.org>
2006-09-07 21:40:45 +0000

    Fix various bugs in feedwise.

--- feedwise.py
+++ feedwise.py
@@ -1,5 +1,5 @@
-# Feedwise Plugin version 0.2 for Rawdog.
-# Ian Glover ian@manicai.net (modified slightly by Adam Sampson)
+# Feedwise Plugin version 0.2+ats2 for Rawdog.
+# Ian Glover ian@manicai.net (modified somewhat by Adam Sampson)
 #
 # Sort articles into chunks by feed rather than date.
 #
@@ -10,16 +10,7 @@ import rawdoglib.plugins
 
 class FeedwisePlugin:
     def __init__(self):
-        last_feed = None
-
-    def startup(self, rawdog, config):
-        # Can't work if daysections or timesections is active.
-        if config["daysections"]:
-            print "Feedwise Plugin: Turning off daysections configuration option."
-            config["daysections"] = 0
-        if config["timesections"]:
-            print "Feedwise Plugin: Turning off timesections configuration option."
-            config["timesections"] = 0
+        self.last_feed = None
 
     # Scan the sorted list of articles and through away old ones to
     # so we don't exceed the configured limit.
@@ -32,13 +23,14 @@ class FeedwisePlugin:
                 feed_counts[feed] = 1
             else:
                 feed_counts[feed] = feed_counts[feed] + 1
-            if feed_counts[feed] > config.get("articles_per_feed", 50):
+            if feed_counts[feed] > config.config.get("articles_per_feed", 50):
                 # We don't want to from the list whilst iterating through it.
                 # So use None as a marker for deletion.
                 to_remove.append(articles[i])
         for x in to_remove:
             articles.remove(x)
-        
+        return True
+
     # Sort the articles by feed and inside each feed by time.
     def sort_by_feed(self, rawdog, config, articles):
         def comparator(lhs, rhs):
@@ -50,37 +42,43 @@ class FeedwisePlugin:
         articles.sort(comparator)
         self.last_feed = None
         self.limit_articles_per_feed(rawdog, config, articles)
-    
+        return False
+
     # Split each feed into its own block.
-    def write_divider(self, rawdog, config, feed, article, bits):
+    def write_divider(self, rawdog, config, f, article, date):
+        feed = rawdog.feeds[article.feed]
+
         # Basic division header.
         basic_divider =  '''<div class="feeddisplay">
-<h3 class="feedtitle">%s</h3><ul class="feedarticles">''' \
-        % feed.get_html_link(config)
-    
+<h3 class="feedtitle">%s</h3>
+<ul class="feedarticles">
+''' % feed.get_html_link(config)
+
         if self.last_feed == None: # First feed.
-            bits['divider'] = basic_divider
+            f.write(basic_divider)
         elif self.last_feed != feed: # A new feed. 
-            bits['divider'] = '</ul></div>\n' + basic_divider
+            f.write('</ul></div>\n' + basic_divider)
         self.last_feed = feed
-    
+        return False
+
     # All the items have been written but we have a couple of tags open so
     # close those.
     def write_end(self, rawdog, config, f):
-        f.write('</ul></div>')
-    
+        if self.last_feed != None:
+            f.write('</ul></div>')
+        return True
+
     # Handle the articles_per_feed configuration option
     def handle_config(self, config, name, value):
         if name == "articles_per_feed":
             config["articles_per_feed"] = int(value)
-            return 0
-        return 1
+            return False
+        return True
 
 
 plugin = FeedwisePlugin()
 
-rawdoglib.plugins.attach_hook( "startup", plugin.startup )
-rawdoglib.plugins.attach_hook( "output_sort", plugin.sort_by_feed )
-rawdoglib.plugins.attach_hook( "output_item_bits", plugin.write_divider )
-rawdoglib.plugins.attach_hook( "output_items_end", plugin.write_end )
-rawdoglib.plugins.attach_hook( "config_option", plugin.handle_config )
+rawdoglib.plugins.attach_hook("output_sort", plugin.sort_by_feed)
+rawdoglib.plugins.attach_hook("output_items_heading", plugin.write_divider)
+rawdoglib.plugins.attach_hook("output_items_end", plugin.write_end)
+rawdoglib.plugins.attach_hook("config_option", plugin.handle_config)
