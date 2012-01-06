1bf78ec4eac66c6185f606e4e0cfab18a266526a
Adam Sampson <ats@offog.org>
2012-01-06 16:34:07 +0000

    Add a documentation comment to backwards.

--- backwards.py
+++ backwards.py
@@ -1,3 +1,9 @@
+"""
+backwards plugin for rawdog, by Adam Sampson <ats@offog.org>
+
+Sort articles in the reverse of the usual order (i.e. oldest first).
+"""
+
 import rawdoglib.plugins
 
 def backwards(rawdog, config, articles):
